30f753cdfc3043a53b9088fd80715e2889df633b
Ryan M <rmccauleymusic@gmail.com>
2019-10-22 01:16:02 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -6,11 +6,11 @@ numbers = [1,2,3]
 def square_array(numbers)
   #numbers = [1,2,3]
   new_numbers  = numbers.each do
-puts numbers * numbers
+puts numbers.to_i * numbers.to_i
   # your code here
   #numbers.each do |numbers|
 end
 end
 #return new_numbers []
 
-#end
+
