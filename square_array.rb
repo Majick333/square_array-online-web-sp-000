13ce9013df834b466e5cdbbdd6089becb829b6bc
Ryan M <rmccauleymusic@gmail.com>
2019-10-22 01:57:56 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -6,7 +6,8 @@ numbers = [1,2,3]
 def square_array(numbers)
   new_numbers = []
    numbers.each do |number|
-    puts [number * number].push
+    puts #[number * number]
+    new_numbers.push(number * number)
 
 return new_numbers
 end
