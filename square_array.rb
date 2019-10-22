3cf2147d4abeb30ff0be2c0b0f254456d8b6a559
Ryan M <rmccauleymusic@gmail.com>
2019-10-22 01:08:05 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 #numbers = [1,2,3]
 def square_array(numbers)
   numbers = [1,2,3]
-  new_numbers  = numbers.each{ |numbers| numbers * numbers}
+  new_numbers  = numbers.each{ |numbers| #{numbers} * #{numbers} }
 return new_numbers
   # your code here
   #numbers.each do |numbers|
